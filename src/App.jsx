040d4e85d2afad19266551ac2a6f6251e0e8da35
Abdullah <112831356+Vega47@users.noreply.github.com>
2025-06-07 12:35:10 +0300

    إصلاح مشاكل الروتنج وإعداد النشر على GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Navbar from "./navbar/navbar";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./home/home";
 import Layout from "./layout/Layout";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import About from "./About/About";
 import Portfolio from "./Portfolio/Portfolio";
 import Contact from "./Contact/Contact";
 import Recipes from "./Recipes/Recipes";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-let route = createBrowserRouter([
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "portfolio", element: <Portfolio /> },
+        { path: "contact", element: <Contact /> },
+        { path: "recipes", element: <Recipes /> },
+      ],
+    },
+  ],
   {
-    path: "",
-    element: <Layout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "/about", element: <About /> },
-      {
-        path: "/portfolio",
-        element: <Portfolio />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/recipes",
-        element: <Recipes />,
-      },
-    ],
-  },
-]);
+    basename: "/SPA-react-js", // تأكد من مطابقة اسم المستودع
+  }
+);
+
 function App() {
-  return (
-    <>
-      <RouterProvider router={route} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
